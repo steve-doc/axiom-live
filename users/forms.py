13d338ec58d1a591c12ee29d0a6ea310a2074968
steve-doc <stevedocherty1@gmail.com>
2023-09-26 17:50:24 +0000

    added profile update form

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
 
 class UserRegisterForm(UserCreationForm):
@@ -10,3 +11,18 @@ class UserRegisterForm(UserCreationForm):
         model = User
         # define the field names and the order they should be displayed
         fields = ['username', 'email', 'password1', 'password2']
+
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta: #subclass that tells django it is the User model we are modifying
+        model = User
+        # define the field names and the order they should be displayed
+        fields = ['username', 'email']
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image']
--- users/views.py
+++ users/views.py
@@ -1,24 +1,44 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
 
 def register(request):
-    if request.method == 'POST': #checks if POST has been received from register.html
-        form = UserRegisterForm(request.POST) #assigns form data to form object
-        if form.is_valid(): #is_valid method checks if form data fields are valid
-            form.save() #saves form to user database
-            #is_valid form data is stored in form.cleaned_data dictionary
-            username = form.cleaned_data.get('username') #assigns stored username to variable
-            #next create success message by injecting username into f string
-            messages.success(request, f'Your account has been created! You are now able to login.') 
-            return redirect('login') #redirects to login page
+    if request.method == 'POST':  # checks if POST has been received from register.html
+        # assigns form data to form object
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():  # is_valid method checks if form data fields are valid
+            form.save()  # saves form to user database
+            # is_valid form data is stored in form.cleaned_data dictionary
+            # assigns stored username to variable
+            username = form.cleaned_data.get('username')
+            # next create success message by injecting username into f string
+            messages.success(
+                request, f'Your account has been created! You are now able to login.')
+            return redirect('login')  # redirects to login page
     else:
-        form = UserRegisterForm() #asssigns blank UserCreation form to form object
+        form = UserRegisterForm()  # asssigns blank UserCreation form to form object
     return render(request, 'users/register.html', {'form': form})
 
 
 @login_required
 def profile(request):
-    return render (request, 'users/profile.html')
+    if request.method == 'POST':
+        update_form = UserUpdateForm(request.POST, instance=request.user)
+        profile_form = ProfileUpdateForm(request.POST,
+                                        request.FILES,
+                                        instance=request.user.profile)
+        if update_form.is_valid() and profile_form.is_valid():
+            update_form.save()
+            profile_form.save()
+            messages.success(request, f'Your profile has been updated!')
+            return redirect('profile')
+    else:
+        update_form = UserUpdateForm(instance=request.user)
+        profile_form = ProfileUpdateForm(instance=request.user.profile)
+    context = {
+        'update_form': update_form,
+        'profile_form': profile_form
+    }
+    return render(request, 'users/profile.html', context)
