d8cafbe020f55ee3ea1ce2fe4c59febd02ac54ad
steve-doc <stevedocherty1@gmail.com>
2023-10-18 13:44:38 +0000

    deployment version

--- axiom/settings.py
+++ axiom/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
+if os.path.isfile("env.py"):
+   import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,14 +24,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-22ohfw&ok$dq8w4%^!p24(6*%#61!v_9qwp^2an4ion#ty2_a8'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-stevedoc-axiom-zkonjehgcr7.ws-eu105.gitpod.io']
+ALLOWED_HOSTS = ['8000-stevedoc-axiom-zkonjehgcr7.ws-eu105.gitpod.io', 'axiom-recruitment-f7943c5e25ff.herokuapp.com']
 
 CSRF_TRUSTED_ORIGINS = ['https://8000-stevedoc-axiom-zkonjehgcr7.ws-eu105.gitpod.io']
 
@@ -83,11 +86,15 @@ WSGI_APPLICATION = 'axiom.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 
---
+++ jobs/static/jobs/scripts/sendEmail.js
@@ -0,0 +1,21 @@
+function sendMail(contactForm) {
+    console.log("Hello")
+    emailjs.send("service_0a3s8xf","template_szeqs67", {
+        "to_email": "sdoc67@gmail.com",
+        "website": "Axiom",
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.email.value,
+        "from_tel": contactForm.tel.value,
+        "interest": contactForm.interest.value
+    })
+    .then(
+        function(response) {
+            contactForm.reset();
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+    return false;  // To block from loading a new page
+}
