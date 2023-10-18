d1e91601f8133110d7393c0c7cce00dcc0454af7
steve-doc <stevedocherty1@gmail.com>
2023-10-18 15:06:56 +0000

    redeploy 6

--- axiom/settings.py
+++ axiom/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-stevedoc-axiom-zkonjehgcr7.ws-eu105.gitpod.io', 'axiom-recruitment-f7943c5e25ff.herokuapp.com']
 
@@ -132,7 +132,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
