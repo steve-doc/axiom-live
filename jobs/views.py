0bd1812128165724a7132511a976ffe2f1ccc880
steve-doc <stevedocherty1@gmail.com>
2023-10-24 20:04:31 +0000

    added fontawsome

--- jobs/views.py
+++ jobs/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import (
     ListView,
