84326491980720b65c23c46523d7afe7e55272b6
steve-doc <stevedocherty1@gmail.com>
2023-10-26 13:57:55 +0000

    Updated footer

--- axiom/settings.py
+++ axiom/settings.py
@@ -30,7 +30,10 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+if os.environ.get('DEVELOPMENT'):
+    DEBUG = True
+else:
+    DEBUG = False
 
 ALLOWED_HOSTS = [
     '8000-stevedoc-axiom-zkonjehgcr7.ws-eu105.gitpod.io',
